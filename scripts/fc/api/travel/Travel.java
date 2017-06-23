b272717b399468a7a2566b1aa41cbf3acb45896c
Final Calibur <fmorris2@oswego.edu>
2017-06-22 21:07:52 -0400

    Added various methods to Travel

--- scripts/fc/api/travel/Travel.java
+++ scripts/fc/api/travel/Travel.java
@@ -1,12 +1,27 @@
 package scripts.fc.api.travel;
 
 import org.tribot.api.interfaces.Positionable;
+import org.tribot.api.types.generic.Condition;
 import org.tribot.api2007.WebWalking;
+
+import scripts.webwalker_logic.WebWalker;
+import scripts.webwalker_logic.local.walker_engine.WalkingCondition;
 
 public class Travel
 {
-	public static boolean travelTo(Positionable p)
+	public static boolean webWalkTo(Positionable p)
 	{
-		return WebWalking.walkTo(p);
+		return !WebWalker.walkTo(p.getPosition()) ? WebWalking.walkTo(p) : true;
+	}
+	
+	public static boolean webWalkTo(Positionable p, Condition c)
+	{
+		return !WebWalker.walkTo(p.getPosition(), () -> {return c.active() ? WalkingCondition.State.EXIT_OUT_WALKER_SUCCESS : WalkingCondition.State.EXIT_OUT_WALKER_FAIL;})
+				? WebWalking.walkTo(p, c, 10) : true;
+	}
+	
+	public static boolean walkToBank()
+	{
+		return !WebWalker.walkToBank() ? WebWalking.walkToBank() : true;
 	}
 }
