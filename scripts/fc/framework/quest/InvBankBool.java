eb15bbb454d4d89d05342123366c576b1f028280
Final Calibur <fmorris2@oswego.edu>
2017-07-05 10:53:43 -0400

    Re-did InvBankBool

--- scripts/fc/framework/quest/InvBankBool.java
+++ scripts/fc/framework/quest/InvBankBool.java
@@ -1,19 +1,44 @@
 package scripts.fc.framework.quest;
 
+import org.tribot.api2007.Inventory;
+
 public class InvBankBool extends QuestBool
 {
 	QuestBool bool;
-	public InvBankBool(int id, int amt, boolean andRelationship, boolean normal)
+	private TYPE type;
+	private int id, amt;
+	public InvBankBool(int id, int amt, TYPE type, boolean normal)
 	{
 		super(normal);
-		bool = andRelationship ? new ItemBool(id, amt, normal).and(new BankBool(id, amt, normal))
-					: new ItemBool(id, amt, normal).or(new BankBool(id, amt, normal));
+		this.type = type;
+		this.id = id;
+		this.amt = amt;
 	}
 
 	@Override
 	public boolean value()
 	{
-		return bool.validate();
+		boolean inBank = !BankBool.bankObserver.hasCheckedBank || BankBool.bankObserver.containsItem(id, amt);
+		boolean inInv = Inventory.getCount(id) >= amt;
+		
+		switch(type)
+		{
+			case NOT_IN_EITHER:
+				return !inBank && !inInv;
+			case IN_ONE:
+				return inBank || inInv;
+			case IN_BOTH:
+				return inBank && inInv;
+			default:
+				return false;
+		}
+	}
+	
+	enum TYPE
+	{
+		NOT_IN_EITHER,
+		IN_ONE,
+		IN_BOTH
 	}
 	
 }
