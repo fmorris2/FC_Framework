e6fd52033af0a96aa986a1e16c0887662a447f30
Final Calibur <fmorris2@oswego.edu>
2017-07-28 14:47:46 -0400

    Added PriceCache for PriceUtils

--- scripts/fc/api/utils/PriceUtils.java
+++ scripts/fc/api/utils/PriceUtils.java
@@ -5,20 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.tribot.api.General;
+
 public class PriceUtils
 {
+	private static final Map<Integer, Integer> PRICE_CACHE = new HashMap<>();
+	
 	public static int getPrice(final int itemId) 
 	{
+		if(PRICE_CACHE.containsKey(itemId))
+			return PRICE_CACHE.get(itemId);
+		
+		General.println("[Pricing] Getting official GE price for item id " + itemId);
 	    try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=" + itemId).openStream()))) {
 	        Matcher matcher = Pattern.compile(".*\"price\":\"?(\\d+\\,?\\.?\\d*)([k|m]?)\"?},\"today\".*").matcher(reader.readLine());
 	        if (matcher.matches()) 
 	        {
 	            double price = Double.parseDouble(matcher.group(1).replace(",",""));
 	            String suffix = matcher.group(2);
-	            return (int) (suffix.isEmpty() ? price : price * (suffix.charAt(0) == 'k' ? 1000 : 1000000));
+	            int parsed = (int) (suffix.isEmpty() ? price : price * (suffix.charAt(0) == 'k' ? 1000 : 1000000));
+	            PRICE_CACHE.put(itemId, parsed);
+	            return parsed;
 	        }
 	    } 
 	    catch (IOException e) 
