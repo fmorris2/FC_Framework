0bc78a7a4099ff38f3e589d33df8f231c70613db
Final Calibur <fmorris2@oswego.edu>
2017-08-10 15:00:39 -0400

    Added ParentTask to framework

---
+++ scripts/fc/framework/task/ParentTask.java
@@ -0,0 +1,37 @@
+package scripts.fc.framework.task;
+
+import java.util.List;
+
+
+public abstract class ParentTask extends Task
+{
+	private static final long serialVersionUID = 3986957991804689745L;
+	protected List<Task> childrenTasks;
+	protected Task currentTask;
+	
+	public ParentTask()
+	{
+		super();
+		childrenTasks = compileChildrenTasks();
+	}
+	
+	protected abstract List<Task> compileChildrenTasks();
+	
+	@Override
+	public boolean execute()
+	{
+		 currentTask = childrenTasks.stream()
+				.filter(t -> t.shouldExecute())
+				.findFirst()
+				.orElse(null);
+		 
+		 return currentTask == null ? false : currentTask.execute();
+	}
+	
+	@Override
+	public String getStatus()
+	{
+		return currentTask == null ? this.getClass().getCanonicalName() : currentTask.getStatus();
+	}
+	
+}
