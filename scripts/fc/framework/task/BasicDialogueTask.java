d654822b6ec15423ac57b83f4bb2d877e5c9f77e
Final Calibur <fmorris2@oswego.edu>
2017-08-10 16:21:47 -0400

    Added BasicDialogueTask

---
+++ scripts/fc/framework/task/BasicDialogueTask.java
@@ -0,0 +1,25 @@
+package scripts.fc.framework.task;
+
+import org.tribot.api.interfaces.Positionable;
+import org.tribot.api2007.Player;
+
+import scripts.fc.api.interaction.EntityInteraction;
+import scripts.fc.api.travel.Travel;
+
+public abstract class BasicDialogueTask extends Task
+{
+	private static final long serialVersionUID = 1L;
+	
+	protected abstract Positionable getPosition();
+	protected abstract int getRadius();
+	protected abstract EntityInteraction getInteraction();
+	
+	@Override
+	public boolean execute()
+	{
+		if(Player.getPosition().distanceTo(getPosition()) > getRadius())
+			return Travel.webWalkTo(getPosition());
+		
+		return getInteraction().execute();
+	}
+}
