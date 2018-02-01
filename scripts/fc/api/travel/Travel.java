0a8a5fb209f637df962adf7eb0505ca37f5e6f93
Final Calibur <fmorris2@oswego.edu>
2018-01-31 19:48:55 -0500

    Walker will now fall back on normal TRiBot web after a certain failure threshold with Dax

--- scripts/fc/api/travel/Travel.java
+++ scripts/fc/api/travel/Travel.java
@@ -2,28 +2,60 @@ package scripts.fc.api.travel;
 
 import org.tribot.api.interfaces.Positionable;
 import org.tribot.api.types.generic.Condition;
+import org.tribot.api2007.WebWalking;
 
 import scripts.webwalker_logic.WebWalker;
 import scripts.webwalker_logic.local.walker_engine.WalkingCondition;
 
 public class Travel
 {
+	private static final int MAX_FAILURES = 5;
+	private static int failures = 0;
 	public static boolean webWalkTo(Positionable p)
 	{
-		return /*!*/WebWalker.walkTo(p.getPosition());// ? WebWalking.walkTo(p) : true;
+		boolean daxSuccess = WebWalker.walkTo(p.getPosition());
+		
+		if(!daxSuccess) {
+			failures++;
+		}
+		
+		if(failures >= MAX_FAILURES) {
+			failures = 0;
+			return WebWalking.walkTo(p);
+		}
+		
+		return daxSuccess;
 	}
 	
 	public static boolean webWalkTo(Positionable p, Condition c)
 	{
-		return WebWalker.walkTo(p.getPosition(), () -> {return c.active() ? WalkingCondition.State.EXIT_OUT_WALKER_SUCCESS : WalkingCondition.State.EXIT_OUT_WALKER_FAIL;});
-		/*
-		return !WebWalker.walkTo(p.getPosition(), () -> {return c.active() ? WalkingCondition.State.EXIT_OUT_WALKER_SUCCESS : WalkingCondition.State.EXIT_OUT_WALKER_FAIL;})
-				? WebWalking.walkTo(p, c, 600) : true;
-		*/
+		boolean daxSuccess = WebWalker.walkTo(p.getPosition(), () -> {return c.active() ? WalkingCondition.State.EXIT_OUT_WALKER_SUCCESS : WalkingCondition.State.EXIT_OUT_WALKER_FAIL;});
+		
+		if(!daxSuccess) {
+			failures++;
+		}
+		
+		if(failures >= MAX_FAILURES) {
+			failures = 0;
+			return WebWalking.walkTo(p, c, 600);
+		}
+		
+		return daxSuccess;
 	}
 	
 	public static boolean walkToBank()
 	{
-		return WebWalker.walkToBank();//!WebWalker.walkToBank() ? WebWalking.walkToBank() : true;
+		boolean daxSuccess = WebWalker.walkToBank();
+		
+		if(!daxSuccess) {
+			failures++;
+		}
+		
+		if(failures >= MAX_FAILURES) {
+			failures = 0;
+			return WebWalking.walkToBank();
+		}
+		
+		return daxSuccess;
 	}
 }
