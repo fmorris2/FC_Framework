ddb557f8a36c08a2bcd91a313637e03399906799
Dad <fmorris2@oswego.edu>
2018-04-16 15:33:45 -0400

    Updated interface ID in condition that checks if the bank is loaded

--- scripts/fc/api/generic/FCConditions.java
+++ scripts/fc/api/generic/FCConditions.java
@@ -329,7 +329,7 @@ public class FCConditions
 	private static Condition bankLoadedCondition()
 	{
 		final int BANK_AMT_MASTER = 12;
-		final int BANK_AMT_CHILD = 5;
+		final int BANK_AMT_CHILD = 16;
 		
 		return new Condition()
 		{
